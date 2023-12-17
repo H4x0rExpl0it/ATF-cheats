44772b217153c892633ea6bfda5af6aaa0ec7ac0
H4x0rExpl0it <126155422+H4x0rExpl0it@users.noreply.github.com>
2023-12-17 22:05:46 +0100

    Update cheats.py

--- cheats.py
+++ cheats.py
@@ -1,9 +1,11 @@
 import keyboard
 from time import sleep
+
 #this is first alpha of ATF cheats 0.1
 print('Welcome to ATF cheats.\nversion: 0.1 alpha\nBasic delay is 4 seconds.\n\n')
 text = str(input('Insert text to type here: '))
 sleep(4)
+
 for character in text:
     keyboard.press(character)
     sleep(0.15)
